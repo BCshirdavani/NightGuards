21d332faab1cce914f7e1ef1b14ce8a1ba6cd471
Beau BCC <beau.shirdavani@bellevuecollege.edu>
2020-11-18 15:29:56 -0800

    Navigation: we have navigation to settings view..
    Problem - the view does not fill the phone screen, we have top and bottom space being unused.

---
+++ NightGuards/ARDisplayView.swift
@@ -0,0 +1,43 @@
+//
+//  ARDisplayView.swift
+//  NightGuards
+//
+//  Created by shy macbook on 11/18/20.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ARDisplayView: View {
+	var body: some View {
+		NavigationView {
+			ARViewContainer()
+		}
+	}
+}
+
+struct ARViewContainer: UIViewRepresentable {
+
+	func makeUIView(context: Context) -> ARView {
+
+		let arView = ARView(frame: .zero)
+
+		// Load the "Box" scene from the "Experience" Reality File
+		let boxAnchor = try! Experience.loadBox()
+
+		// Add the box anchor to the scene
+		arView.scene.anchors.append(boxAnchor)
+
+		return arView
+
+	}
+
+	func updateUIView(_ uiView: ARView, context: Context) {}
+
+}
+
+struct ARDisplayView_Previews: PreviewProvider {
+    static var previews: some View {
+        ARDisplayView()
+    }
+}
---
+++ NightGuards/SettingsUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsUIView.swift
+//  NightGuards
+//
+//  Created by shy macbook on 11/18/20.
+//
+
+import SwiftUI
+
+struct SettingsUIView: View {
+    var body: some View {
+        Text("Settings View")
+    }
+}
+
+struct SettingsUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsUIView()
+    }
+}
