6109225f73edfdc4ee41a3cff6be355c3813d1f5
ShiMac <beau.shirdavani@bellevuecollege.edu>
2021-01-16 11:14:06 -0800

    settings page has popup for credits.

--- NightGuards/View/SettingsUIView.swift
+++ NightGuards/View/SettingsUIView.swift
@@ -9,36 +9,46 @@ import SwiftUI
 
 struct SettingsUIView: View {
 	@State private var isOnSetting1 = false
+    @State private var showCreditsModal = false
     let purchaser = Purchaser()
 
-	var body: some View {
-		List{
-			Toggle(isOn: $isOnSetting1) {
-				Text("setting 1")
-			}
-			.padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-			HStack {
-				Text("restore purchases?")
-				Spacer()
-				Section {
+    var body: some View {
+        List{
+            HStack {
+                Text("restore purchases?")
+                Spacer()
+                Section {
                     Button(action: {purchaser.restore()}) {
-						Text("Restore").padding(10)
-					}
-					.buttonStyle(PlainButtonStyle())
-					.cornerRadius(10)
-					.foregroundColor(.white)
-					.background(Color.green)
-					.accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-				}
+                        Text("Restore").padding(10)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                }
             }
-			.padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-			Button(action: {print("test pressed")}) {
-				Text("test")
-			}
-			.padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-		}
-		.navigationBarTitle(Text("Settings"), displayMode: .inline)
-	}
+            .padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            Text("Credits").onTapGesture(perform: {
+                showCreditsModal = true
+            }).sheet(isPresented: $showCreditsModal, onDismiss: {showCreditsModal = false}, content: {
+                VStack {
+                    ScrollView {
+                        Text("dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  ")
+                    }
+                    Button(action: {showCreditsModal = false}){
+                        Text("Close").padding(10)
+                    }.buttonStyle(PlainButtonStyle())
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    .padding(5)
+                }
+            })
+        }
+        .navigationBarTitle(Text("Settings"), displayMode: .inline)
+    }
 }
 
 struct SettingsUIView_Previews: PreviewProvider {
