45d4c964f2dd764ff3483095eed817f9968c46fa
Beau BCC <beau.shirdavani@bellevuecollege.edu>
2021-01-17 14:06:18 -0800

    deleted old Reality Experiences from Assets folder. Updated Credits with dummy text.

--- NightGuards/View/SettingsUIView.swift
+++ NightGuards/View/SettingsUIView.swift
@@ -12,43 +12,68 @@ struct SettingsUIView: View {
     @State private var showCreditsModal = false
     let purchaser = Purchaser()
 
-    var body: some View {
-        List{
-            HStack {
-                Text("restore purchases?")
-                Spacer()
-                Section {
-                    Button(action: {purchaser.restore()}) {
-                        Text("Restore").padding(10)
-                    }
-                    .buttonStyle(PlainButtonStyle())
-                    .cornerRadius(10)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                }
-            }
-            .padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Text("Credits").onTapGesture(perform: {
-                showCreditsModal = true
-            }).sheet(isPresented: $showCreditsModal, onDismiss: {showCreditsModal = false}, content: {
-                VStack {
-                    ScrollView {
-                        Text("dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  dfs sdaf asdf asdf sadf sadf sadf sadf asdf asdf asdf sad sdaf sdaf asdf sdaf sdaf asdf asdf asdf asdf asdf asdf sadf asdf asdf asdf sadf asdf asdf asdf asdf asdf sadf asdf asdf asdf asdf asdf asdf asdf asdf  ")
-                    }
-                    Button(action: {showCreditsModal = false}){
-                        Text("Close").padding(10)
-                    }.buttonStyle(PlainButtonStyle())
-                    .cornerRadius(10)
-                    .foregroundColor(.white)
-                    .background(Color.red)
-                    .accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    .padding(5)
-                }
-            })
-        }
-        .navigationBarTitle(Text("Settings"), displayMode: .inline)
-    }
+	var body: some View {
+		List{
+			HStack {
+				Text("restore purchases?")
+				Spacer()
+				Section {
+					Button(action: {purchaser.restore()}) {
+						Text("Restore").padding(10)
+					}
+					.buttonStyle(PlainButtonStyle())
+					.cornerRadius(10)
+					.foregroundColor(.white)
+					.background(Color.green)
+					.accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+				}
+			}
+			.padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+			Text("Credits").onTapGesture(perform: {
+				showCreditsModal = true
+			}).sheet(isPresented: $showCreditsModal, onDismiss: {showCreditsModal = false}, content: {
+				VStack {
+					ScrollView {
+						Text("Credits for 3D models and Characters:").font(.title)
+						Group{
+							Group {Text("Lucha").font(.headline)
+								Text("From Mixamo.com, character name Ortiz")}
+							Group {Text("Donut").font(.headline)
+								Text("\"Doughnut\" (https://skfb.ly/6RqTy) by ivan_the_terrible is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).")}
+							Group {Text("Alex").font(.headline)
+								Text("\"Minecraft Alex\" (https://skfb.ly/YFzT) by HyperOwl is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).")}
+							Group {Text("Alex").font(.headline)
+								Text("\"Minecraft Alex\" (https://skfb.ly/YFzT) by HyperOwl is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).")}
+							Group {Text("Alex").font(.headline)
+								Text("\"Minecraft Alex\" (https://skfb.ly/YFzT) by HyperOwl is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).")}
+						}
+						Group {
+							Group {Text("CREEPER").font(.headline)
+								Text("\"Minecraft - Creeper\" (https://skfb.ly/6QTSz) by Vincent Yanez is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).")}
+							Group {Text("ENDERMAN").font(.headline)
+								Text("\"Minecraft - Enderman\" (https://skfb.ly/6R7vR) by Vincent Yanez is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).")}
+							Group {Text("Villager").font(.headline)
+								Text("\"Minecraft - Villager\" (https://skfb.ly/6QTPw) by Vincent Yanez is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).")}
+							Group {Text("Alex").font(.headline)
+								Text("\"Minecraft Alex\" (https://skfb.ly/YFzT) by HyperOwl is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).")}
+							Group {Text("Alex").font(.headline)
+								Text("\"Minecraft Alex\" (https://skfb.ly/YFzT) by HyperOwl is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).")}
+						}
+
+					}
+					Button(action: {showCreditsModal = false}){
+						Text("Close").padding(10)
+					}.buttonStyle(PlainButtonStyle())
+					.cornerRadius(10)
+					.foregroundColor(.white)
+					.background(Color.red)
+					.accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+					.padding(5)
+				}.padding(5)
+			})
+		}
+		.navigationBarTitle(Text("Settings"), displayMode: .inline)
+	}
 }
 
 struct SettingsUIView_Previews: PreviewProvider {
